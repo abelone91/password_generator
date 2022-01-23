bb42fcfe0322639830bd7b6e60754c939f333eba
abelone91 <abel.arrouj@gmail.com>
2022-01-23 15:23:20 +0100

    passgen

---
+++ main.py
@@ -0,0 +1,84 @@
+# pass generator using Tkinter
+
+from tkinter import *
+from random import randint
+
+root = Tk()
+
+
+
+# window title
+
+root.title('Password Generator')
+
+# window size
+root.geometry("600x400")
+
+# Generate Random Strong Password
+def new_rand():
+	# Clear Our Entry Box
+	return_entry.delete(0, END)
+
+	# Get PW Length and convert to integer
+	try:
+		length = int(my_entry.get())
+
+	except:
+
+		error_msg.config(text="Warning: Invalid input. Input should only contain digits!")
+
+
+
+	# create a variable to hold our password
+	my_password = ''
+
+	# Loop through password length
+	for x in range(length):
+		my_password += chr(randint(33,126))
+
+	# Output password to the screen
+	return_entry.insert(0, my_password)
+
+
+# Copy to clipboard
+def clipper():
+	# Clear the clipboard
+	root.clipboard_clear()
+	# Copy to clipboard
+	root.clipboard_append(pw_entry.get())
+
+# Label
+prompt = Label(text="How many characters will the passord contain?", font=("Calibre", 15)).pack(pady=20)
+
+
+
+# Create Entry Box To Designate Number of Characters
+my_entry = Entry(root, font=("Calibre", 20))
+my_entry.pack(pady=20, padx=20)
+
+# Create Entry Box For Our Returned Password
+return_entry = Entry(root, text="", font=("Calibre", 20), bd=0, bg="systembuttonface")
+return_entry.pack(pady=20)
+
+# Create a frame for our Buttons
+my_frame = Frame(root)
+my_frame.pack(pady=20)
+
+# Buttons
+my_button = Button(my_frame, text="Generate Password", command=new_rand)
+my_button.grid(row=0, column=0, padx=10)
+
+copy_button = Button(my_frame, text="Copy To Clipboad", command=clipper)
+copy_button.grid(row=0, column=1, padx=10)
+
+quit = Button(my_frame, text="Quit", command=root.destroy)
+quit.grid(row=0, column=2, padx=10)
+
+#error message
+
+error_msg = Label(root, text="",font=("Calibre", 10), bd=0, bg="systembuttonface")
+error_msg.pack(pady=10)
+
+
+root.mainloop()
+
